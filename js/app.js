8b752ae57bd9c3d8f5b3d90f652a7a09592df965
Maria Cecília <o.cecilia@aluno.ifsp.edu>
2025-03-31 17:50:22 -0300

    Criando materias model e service

--- js/app.js
+++ js/app.js
@@ -1,40 +1,17 @@
-const alunos = [
-	{
-	  nome: "chico melato",
-	  notas: {
-		backend_1: [7, 8.5, 9, 6.5],
-		frontend_2: [6, 8, 5, 7],
-		bancodados: [7, 7, 6.5, 8],
-		ferramentas: [3, 3, 3, 3],
-	  },
-	},
-	{
-	  nome: "talita lima",
-	  notas: {
-		backend_1: [4, 4, 4, 4],
-		frontend_2: [4, 4, 5, 5],
-		bancodados: [5, 5, 6, 6],
-		ferramentas: [7, 7, 8, 9],
-	  },
-	},
-	{
-	  nome: "Fulano da Silva",
-	  notas: {
-		backend_1: [4, 4, 4, 4],
-		frontend_2: [4, 4, 5, 5],
-		bancodados: [5, 5, 6, 6],
-		ferramentas: [7, 7, 8, 9],
-	  },
-	},
-];
-  
+const materiaService = new MateriaService()
+let materias = materiaService.getAll()
+if(materias.length === 0){
+	materiaService.add(new MateriaModel({nome: 'Back-End 1'}))
+	materiaService.add(new MateriaModel({nome: 'Front-End 2'}))
+	materiaService.add(new MateriaModel({nome: 'Banco de Dados'}))
+	materiaService.add(new MateriaModel({nome: 'Ferramentas Web'}))
+}
+
 const alunoService = new AlunoService()
 
-alunos.forEach(aluno => {
-	alunoService.add(new AlunoModel(aluno))
-})
-
-const alunoView = new AlunoView(document.querySelector('[data-table-alunos]'))
+const alunoView = new AlunoView(
+	document.querySelector('[data-table-alunos]'), 
+	materiaService.getAll())
 const alunoController = new AlunoController(alunoService, alunoView)
 
 document.querySelector('form').addEventListener('submit', function (event) {
